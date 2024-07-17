886f9e3e4450556b209a4c3dc8a570266b4cfc2e
Gorway <gor.petrosyan.alexandri@gmail.com>
2024-07-17 22:50:41 +0400

    Resolve merge conflicts

--- Task_7.1-7.7/Task_7-4.py
+++
@@ -1,9 +0,0 @@
-# Task 7-4
-
-string = "reverse me"
-reversed_string = ""
-
-for i in range(len(string) -1, -1, -1): # or reversed_string = string[::-1]
-    reversed_string += string[i]
-
-print(reversed_string)
